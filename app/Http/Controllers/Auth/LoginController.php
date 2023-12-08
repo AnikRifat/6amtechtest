eb96176a27006466f836a13b03b7661faf4b5b9a
Anik Rifat <reafatul@gmail.com>
2023-12-08 18:38:12 +0600

    update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -38,12 +38,13 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
     protected function credentials(Request $request)
     {
         if (is_numeric(request()->get('email'))) {
             return ['phone' => $request->get('email'), 'password' => $request->get('password')];
         }
+
         return $request->only($this->username(), 'password');
     }
-
 }
